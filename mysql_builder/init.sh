0899fdab0defe7aab6ed3d0042535e40ef8b9c54
narsonos <narsonos39@gmail.com>
2025-09-24 01:05:43 +0200

    feat: integration test suite including a set of fixtures and test-compose, a mysql baker compose and dockerfile for creating a 'golden' mysql image which has been initialized already for faster execution. Smarter UoW now memorizes event loops and RedisConnecitonManager that ensure safe async execution.

---
+++ mysql_builder/init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# /mysql_builder_final/init.sh
+set -e
+
+/usr/local/bin/docker-entrypoint.sh mysqld --default-authentication-plugin=caching_sha2_password &
+
+echo "Waiting for MySQL to become ready..."
+until mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "SELECT 1" >/dev/null 2>&1; do
+  sleep 1
+done
+
+
+echo "MySQL is ready. Creating user and database..."
+mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`; CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}'; GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';"
+echo "MySQL is ready! Shutting down..."
+
+mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
